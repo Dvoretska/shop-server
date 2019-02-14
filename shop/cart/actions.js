b955a8922bec6e1a2696315864336751dc0aaa4b
Dasha <dvoretska4@gmail.com>
2019-02-14 14:25:22 +0200

    Refactor carts actions.

--- shop/cart/actions.js
+++ shop/cart/actions.js
@@ -66,89 +66,106 @@ async function addProductToCart(req, res, next) {
   }
 }
 
-function getCart(req, res, next) {
-  Cart.where({user_id: req.user.attributes.id})
-      .query('orderBy', 'quantity', 'desc')
-      .fetchAll({withRelated: ['product_id']})
-      .then(products => {
-        if(!products) {
-          return next();
-        }
-        Category.forge().fetchAll().then(categories => {
-          let cartArr = [];
-          products.map((item) => {
-            var cart = {};
-            let category = categories.find(o =>  o.id === +item.relations.product_id.attributes.category_id);
-            cart['category'] = category.attributes.name;
-            cart['id'] = item.attributes.id;
-            cart['size'] = item.attributes.size;
-            cart['product_id'] = item.relations.product_id.attributes.id;
-            cart['quantity'] = item.attributes.quantity;
-            cart['brand'] = item.relations.product_id.attributes.brand;
-            cart['price'] = item.relations.product_id.attributes.price;
-            if(item.relations.product_id.attributes.discount) {
-              cart['discount'] = item.relations.product_id.attributes.discount;
-              cart['amount'] = cart['quantity'] * cart['discount'];
-            } else {
-              cart['amount'] = cart['quantity'] * cart['price'];
-            }
-            cartArr.push(cart);
-          });
-          Image.forge().fetchAll({withRelated: 'product'}).then(images => {
-            handleImagesTable.addImagesToResult(images, cartArr, 'product_id');
-            let totalNumberOfProducts = summary.calcTotalNumberOfProducts(products);
-            let totalAmount = summary.calcTotalAmount(products);
-            return res.status(200).send({cart: cartArr, totalAmount, totalNumberOfProducts})
-          })
-        })
-      })
+async function getCart(req, res, next) {
+  try {
+    let cart = await Cart.where({user_id: req.user.attributes.id}).query('orderBy', 'quantity', 'desc')
+      .fetchAll({withRelated: ['product_id.subcategory.category', 'size_id']});
+    if (!cart) {
+      return next();
+    }
+    let cartArr = [];
+    cart.map((cart_item) => {
+      var cart_obj = {};
+      cart_obj['category'] = cart_item.relations.product_id.relations.subcategory.relations.category.attributes.name;
+      cart_obj['id'] = cart_item.attributes.id;
+      cart_obj['size'] = cart_item.relations.size_id.attributes.name;
+      cart_obj['size_id'] = cart_item.relations.size_id.attributes.id;
+      cart_obj['product_id'] = cart_item.relations.product_id.attributes.id;
+      cart_obj['quantity'] = cart_item.attributes.quantity;
+      cart_obj['brand'] = cart_item.relations.product_id.attributes.brand;
+      cart_obj['price'] = cart_item.relations.product_id.attributes.price;
+      if (cart_item.relations.product_id.attributes.discount) {
+        cart_obj['discount'] = cart_item.relations.product_id.attributes.discount;
+        cart_obj['amount'] = cart_obj['quantity'] * cart_obj['discount'];
+      } else {
+        cart_obj['amount'] = cart_obj['quantity'] * cart_obj['price'];
+      }
+      cartArr.push(cart_obj);
+    });
+    let images = await Image.forge().fetchAll({withRelated: 'product'});
+    handleImagesTable.addImagesToResult(images, cartArr, 'product_id');
+    let totalNumberOfProducts = summary.calcTotalNumberOfProducts(cart);
+    let totalAmount = summary.calcTotalAmount(cart);
+    return res.status(200).send({cart: cartArr, totalAmount, totalNumberOfProducts})
+  }
+  catch(err) {
+    return next(err);
+  }
 }
 
-function decreaseQuantityOfProductInCart(req, res) {
-  Cart.forge({product_id: req.body.product_id, size: req.body.size, user_id: req.user.attributes.id}).query('orderBy', 'id', 'desc').fetch({withRelated: ['product_id']}).then((product) => {
-    let quantity = +product.attributes.quantity - 1;
-    Cart.where({product_id: req.body.product_id, size: product.attributes.size})
-        .save({quantity: quantity}, {patch: true})
-        .then((result) => {
-          let amount = 0;
-          if(product.relations.product_id.attributes.discount) {
-            amount = product.relations.product_id.attributes.discount * quantity;
-          } else {
-            amount = product.relations.product_id.attributes.price * quantity;
-          }
-          Cart.where({user_id: req.user.attributes.id}).fetchAll({withRelated: ['product_id']}).then(total => {
-            let totalAmount = summary.calcTotalAmount(total);
-            let totalNumberOfProducts = summary.calcTotalNumberOfProducts(total);
-            return res.status(201).send({product, productQty: result, amount, totalAmount, totalNumberOfProducts});
-          }).catch(err => {
-            return res.status(400).send(err)
-          })
-        });
-  })
+async function decreaseQuantityOfProductInCart(req, res, next) {
+  let data = {
+    product_id: req.body.product_id,
+    size_id: req.body.size_id,
+    user_id: req.user.attributes.id
+  };
+  let dataForStock = {
+    product_id: req.body.product_id,
+    size_id: req.body.size_id
+  };
+  try {
+    let stock = await Stock.where(dataForStock).fetch();
+    let stock_quantity = stock.attributes.quantity + 1;
+    await Stock.where(dataForStock).save({quantity: stock_quantity}, {patch: true});
+    let cart = await Cart.forge(data).fetch({withRelated: ['product_id']});
+    let cart_quantity = cart.attributes.quantity - 1;
+    let cart_quantity_updated = await Cart.where(data).save({quantity: cart_quantity}, {patch: true});
+    let cart_updated = await Cart.where(data).fetch();
+    let all_cart = await Cart.where({user_id: req.user.attributes.id}).fetchAll({withRelated: ['product_id']});
+    let amount = 0;
+    if (cart.relations.product_id.attributes.discount) {
+      amount = cart.relations.product_id.attributes.discount * cart_updated.attributes.quantity;
+    } else {
+      amount = cart.relations.product_id.attributes.price * cart_updated.attributes.quantity;
+    }
+    let totalAmount = summary.calcTotalAmount(all_cart);
+    let totalNumberOfProducts = summary.calcTotalNumberOfProducts(all_cart);
+    return res.status(201).send({
+      product: cart_updated,
+      productQty: cart_quantity_updated,
+      amount,
+      totalAmount,
+      totalNumberOfProducts
+    });
+  }
+  catch (err) {
+    return next(err);
+  }
 }
 
-function deleteProductFromCart(req, res) {
-  Cart.where({id: req.body.id, user_id: req.user.attributes.id}).destroy().then((product) => {
-    Cart.where({user_id: req.user.attributes.id}).fetchAll({withRelated: ['product_id']}).then(result => {
-      let totalAmount = summary.calcTotalAmount(result);
-      let totalNumberOfProducts = summary.calcTotalNumberOfProducts(result);
-      return res.status(201).send({id: req.body.id, totalAmount, totalNumberOfProducts});
-    }).catch(err => {
-      return res.status(400).send(err)
-    })
-  })
+async function deleteProductFromCart(req, res, next) {
+  try {
+     await Cart.where({id: req.body.id, user_id: req.user.attributes.id}).destroy();
+     let cart = await Cart.where({user_id: req.user.attributes.id}).fetchAll({withRelated: ['product_id']});
+     let totalAmount = summary.calcTotalAmount(cart);
+     let totalNumberOfProducts = summary.calcTotalNumberOfProducts(cart);
+     return res.status(201).send({id: req.body.id, totalAmount, totalNumberOfProducts});
+  }
+  catch(err) {
+    return next(err);
+  }
 }
 
-function getTotalNumberOfProductsInCart(req, res) {
-  Cart.where({user_id: req.user.attributes.id}).fetchAll({withRelated: ['product_id']}).then(total => {
-    let totalNumberOfProducts = summary.calcTotalNumberOfProducts(total);
+async function getTotalNumberOfProductsInCart(req, res, next) {
+  try {
+    let cart = Cart.where({user_id: req.user.attributes.id}).fetchAll({withRelated: ['product_id']});
+    let totalNumberOfProducts = summary.calcTotalNumberOfProducts(cart);
     return res.status(201).send({totalNumberOfProducts});
-  }).catch(err => {
-    return res.status(400).send(err)
-  })
+  }
+  catch(err) {
+    return next(err);
+  }
 }
-
-
 
 module.exports = {
   addProductToCart,
